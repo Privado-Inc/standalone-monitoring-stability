3f9515ddd4fa51d424a7f7120d9c12b6163fbe25
Ankit Kumar <ankit.kumar@privado.ai>
2022-12-05 08:10:07 +0530

    Added Stable and Dev json file generation support

--- utils/clone-repo.py
+++ utils/clone-repo.py
@@ -2,7 +2,7 @@ from git.repo.base import Repo
 import os
 
 def main():
-    clone_repo(['https://github.com/saurabh-sudo/BankingSystem-Backend'])
+    clone_repo('https://github.com/saurabh-sudo/BankingSystem-Backend')
     install_privado()
 
 def install_privado():
@@ -14,14 +14,13 @@ def install_privado():
     else:
         os.system("curl -o- https://raw.githubusercontent.com/Privado-Inc/privado-cli/main/install.sh | bash")
 
-def clone_repo(urls):
+def clone_repo(url):
 
-    for url in urls:
-        repo_name = url.split("/")[-1]
-        location = "./repos/" + repo_name
-        print("Cloning the Repo: " + repo_name)
-        Repo.clone_from(url, location)
-        print("Cloning Successful: " + repo_name)
+    repo_name = url.split("/")[-1]
+    location = "./repos/" + repo_name
+    print("Cloning the Repo: " + repo_name)
+    Repo.clone_from(url, location)
+    print("Cloning Successful: " + repo_name)
     
     print("All repos cloned.")
 
--- utils/scan.py
+++ utils/scan.py
@@ -1,22 +1,39 @@
 import os
 import subprocess
+import shutil
 
 def main():
     scan_repo_report()
 
 def scan_repo_report():
     repos = get_list_repos()
+    cwd = os.getcwd()
 
-    for i in repos:
-        privado_dir = os.path.join(os.path.expanduser('~'), ".privado/bin/privado")
-        scan_dir = './repos/' + i
-        print(privado_dir + " scan --skip-upload " + scan_dir)
-        os.system(privado_dir + " scan --skip-upload " + scan_dir)
+    print(cwd)
+
+    privado_dir = os.path.join(os.path.expanduser('~'), ".privado/bin")
+    scan_dir = cwd + '/repos/' + repos[0]
+
+    # Scan the cloned repo with stable
+    os.system(privado_dir + "/privado scan --skip-upload " + scan_dir)
+
+    # Move the privado.json file to the result folder as stable.json
+    shutil.move(cwd + '/repos/' + repos[0] + '/.privado/privado.json', cwd + '/result/stable.json')
+
+    # Scan the cloned repo with dev
+    os.system('PRIVADO_DEV=1 ' + privado_dir + '/privado scan --overwrite --skip-upload ' + scan_dir)
+
+    # # Move the privado.json file to the result folder as dev.json
+    shutil.move(cwd + '/repos/' + repos[0] + '/.privado/privado.json', cwd + '/result/dev.json')
 
 # Return list of cloned repo name
 def get_list_repos():
+    result = []
     repos = os.listdir("./repos")
-    return repos
+    for i in repos:
+        if not i.startswith('.'):
+            result.append(i)
+    return result
 
 if __name__ == "__main__":
     main()
