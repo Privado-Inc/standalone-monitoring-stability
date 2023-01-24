3dcd796630137fd73eda19087f23a58f211b24a7
Karan <karan.batavia@privado.ai>
2023-01-25 00:05:34 +0530

    added comments for subscan times

--- utils/subscan_times.py
+++ utils/subscan_times.py
@@ -14,12 +14,12 @@ def get_tag_time_pairs(filepath):
 
                 tag_separated_list = list(
                     filter(
-                        lambda x: not re.match(r"^\s+$", x) and len(x), line.split('-'))) # because the line is of the form "Tag key - zzMS" 
+                        lambda x: not re.match(r"^\s+$", x) and len(x), line.split('-'))) # because the line is of the form "Tag key - zzMS", remove all the spaces and tabs
 
-                tag_time_pair = tag_separated_list[1:3]
+                tag_time_pair = tag_separated_list[1:3] # Only consider the tag names and time required, Ex - Language Detection done in and ZZms
 
                 
-                yield tuple(tag_time_pair)
+                yield tuple(tag_time_pair) # Generate output for each line
 
 
 def get_subscan_times(branch):
@@ -28,8 +28,8 @@ def get_subscan_times(branch):
 
     for tag_time_pair in get_tag_time_pairs(filepath):
         tag = re.sub(pattern=r"(\t|done in|is done in)",repl="", string=tag_time_pair[0]).strip()
-        time = tag_time_pair[1].strip()
-        subscan_map[tag] = time
+        time = tag_time_pair[1].strip() 
+        subscan_map[tag] = time # Map all the tags to the times in a dictionary
     
     return subscan_map
     
