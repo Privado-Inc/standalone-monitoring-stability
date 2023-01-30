5874783d7ca20e74aaed3ad1a2e4d53452dd6724
Karan <karan.batavia@privado.ai>
2023-01-30 15:09:22 +0530

    support for separate performance report

--- utils/write_to_file.py
+++ utils/write_to_file.py
@@ -1,14 +1,14 @@
 import csv
 import os
 
-def write_to_csv(filename, headers, values):
+def write_to_csv(filename, headers, values, insert_headers=False): 
     # filename is the name of the granular report
     # headers is an array containing the column names -
     # values is a 2D Array with each element signifying a particular row
     cwd = os.getcwd()
     with open(f'{cwd}/{filename}.csv', "a") as value:
         report = csv.writer(value)
-        report.writerow(headers)
+        if (insert_headers): report.writerow(headers) 
         for row in values:
             report.writerow(row)
 
