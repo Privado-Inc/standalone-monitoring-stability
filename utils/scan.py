1174ba99fe4a610a772bd772c8ea4b1e1e013664
Karan <karan.batavia@privado.ai>
2023-02-03 14:11:43 +0530

    scan status report fix

--- utils/scan.py
+++ utils/scan.py
@@ -106,16 +106,20 @@ def generate_scan_status_data(scan_status, first_branch, second_branch):
         scan_metadata_regex = r".*(code scanning|binary file size|Deduplicating flows is done in).*"
         scan_metadata_values = []
 
-        with open(f"{cwd}/temp/result/{first_branch}/output.txt") as scan_time_output:
+        with open(f"{cwd}/temp/result/{first_branch}/{repo}-output.txt") as scan_time_output:
             for line in scan_time_output.readlines():
                 if (re.search(scan_metadata_regex, line)):
                     scan_metadata_values.push(line)
 
-        with open(f"{cwd}/temp/result/{second_branch}output.txt") as scan_time_output:
+        with open(f"{cwd}/temp/result/{second_branch}/{repo}-output.txt") as scan_time_output:
             for line in scan_time_output.readlines():
                 if (re.search(scan_metadata_regex, line)):
                     scan_metadata_values.push(line)
 
+        unique_flows = ""
+        code_scan_time = ""
+        binary_file_size = ""
+        
         try:
             unique_flows = scan_metadata_values[0].split('-')[-1] 
             code_scan_time = scan_metadata_values[1].split('-')[-2]
