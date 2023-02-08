1e07d0c8368bf28f7126c3274f85ad6b9550ba05
Ankit Kumar <ankit.kumar@privado.ai>
2023-02-08 15:46:45 +0530

    test

--- aws-export.py
+++ aws-export.py
@@ -14,8 +14,8 @@ def main():
 
     #os.system(f"mv output.xlsx output{append_name}.xlsx")
     #os.system(f"aws s3 cp output{append_name}.xlsx s3://privado-testing")
-    os.system(f"cd temp && sudo zip -r result{append_name}.zip result")
-    os.system(f"aws s3 cp result{append_name}.zip s3://privado-testing")
+    #os.system(f"cd temp && sudo zip -r result{append_name}.zip result")
+    os.system(f"cd temp && aws s3 cp result{append_name}.zip s3://privado-testing")
 
     print("Successfully uploaded to s3 with name")
 
