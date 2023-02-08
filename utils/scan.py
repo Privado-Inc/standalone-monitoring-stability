f0cc47075303c2629d71fa2577a541ef57f48115
Ankit Kumar <ankit.kumar@privado.ai>
2023-02-08 07:45:41 +0530

    test

--- utils/scan.py
+++ utils/scan.py
@@ -7,67 +7,78 @@ import re
 
 
 def get_docker_commands(tag, repo_path):
-    if (tag == 'main'):
+    if tag == 'main':
         return f'privado scan {repo_path}'
-    elif (tag == 'dev'):
+    elif tag == 'dev':
         return f'PRIVADO_DEV=1 privado scan {repo_path}'
     else:
         return f'PRIVADO_DEV=1 PRIVADO_TAG={tag} privado scan {repo_path}'
 
-def scan_repo_report(first_branch, second_branch, use_docker):
-    repos = get_list_repos()
+
+def scan_repo_report(first_branch, second_branch, valid_repos, use_docker):
     cwd = os.getcwd()
 
-    scan_status = dict() # To store scan status - if it failed or completed, and for which branch 
+    # To store scan status - if it failed or completed, and for which branch
+    scan_report = dict()
 
     # create dirs for results if not exist
-    if not os.path.isdir(cwd + '/temp/result/' + first_branch): os.system('mkdir -p ' + cwd + '/temp/result/' + first_branch)
-    if not os.path.isdir(cwd + '/temp/result/' + second_branch): os.system('mkdir -p ' + cwd + '/temp/result/' + second_branch)  
-    if not os.path.isdir(cwd + '/temp/cpu_mem'): os.system('mkdir -p ' + cwd + '/temp/cpu_mem')
+    if not os.path.isdir(cwd + '/temp/result/' + first_branch):
+        os.system('mkdir -p ' + cwd + '/temp/result/' + first_branch)
+    if not os.path.isdir(cwd + '/temp/result/' + second_branch):
+        os.system('mkdir -p ' + cwd + '/temp/result/' + second_branch)
 
-    for repo in repos:         
+    for repo in valid_repos:
         scan_dir = cwd + '/temp/repos/' + repo
         try:
             # Scan the cloned repo with first branch and push output to a file
-            if (use_docker):
+            if use_docker:
                 first_command = f'{get_docker_commands(first_branch, scan_dir)} | tee {cwd}/temp/result/{first_branch}/{repo}-output.txt'
             else:
-                first_command = f'cd {cwd}/temp/binary/{first_branch}/bin && ({{ time ./privado-core scan {scan_dir} -ic {cwd}/temp/privado --skip-upload -Dlog4j.configurationFile=log4j2.xml ; }} 2> {cwd}/temp/result/{first_branch}/{repo}_time.txt | tee {cwd}/temp/result/{first_branch}/{repo}-output.txt)'
+                first_command = f'cd {cwd}/temp/binary/{first_branch}/bin && ./privado-core scan {scan_dir} -ic {cwd}/temp/privado --skip-upload -Dlog4j.configurationFile=log4j2.xml | tee {cwd}/temp/result/{first_branch}/{repo}-output.txt'
             
             # Execute the command to generate the binary file for first branch
             os.system(first_command)
 
-            # Move the privado.json file to the result folder
             src_path = f'{scan_dir}/.privado/privado.json'
-
             dest_path = f'{cwd}/temp/result/{first_branch}/{repo}.json'
+
+            report = []
+
+            # Move the privado.json file to the result folder
             try:
                 shutil.move(src_path, dest_path)
-                scan_status[f"{repo},{first_branch}"] = "done"
+                report[first_branch] = {'scan_status': 'done', 'scan_error_message': '--'}
+                # scan_status[f"{repo},{first_branch}"] = "done"
             except Exception as e:
-                scan_status[f"{repo},{first_branch}"] = f'failed,{str(e)}'
+                report[first_branch] = {'scan_status': 'failed', 'scan_error_message': e}
+                # scan_status[f"{repo},{first_branch}"] = f'failed,{str(e)}'
 
-            if (use_docker):
+            # Scan the cloned repo with second branch and push output to a file with debug logs
+            if use_docker:
                 second_command = f'{get_docker_commands(second_branch, scan_dir)} | tee {cwd}/temp/result/{second_branch}/{repo}-output.txt'
             else:
-                # Scan the cloned repo with second branch and push output to a file with debug logs
-                second_command = f'cd {cwd}/temp/binary/{second_branch}/bin && ({{ time ./privado-core scan {scan_dir} -ic {cwd}/temp/privado --skip-upload -Dlog4j.configurationFile=log4j2.xml ; }} 2> {cwd}/temp/result/{second_branch}/{repo}_time.txt | tee {cwd}/temp/result/{second_branch}/{repo}-output.txt)'
+                second_command = f'cd {cwd}/temp/binary/{second_branch}/bin && ./privado-core scan {scan_dir} -ic {cwd}/temp/privado --skip-upload -Dlog4j.configurationFile=log4j2.xml | tee {cwd}/temp/result/{second_branch}/{repo}-output.txt'
             
             # Execute the command to generate the binary file for second branch
             os.system(second_command)
 
             dest_path = f'{cwd}/temp/result/{second_branch}/{repo}.json'   
             try:
-                shutil.move(src_path,dest_path)
-                scan_status[f"{repo},{second_branch}"] = "done"
+                shutil.move(src_path, dest_path)
+                report[second_branch] = {'scan_status': 'done', 'scan_error_message': '--'}
+                # scan_status[f"{repo},{second_branch}"] = "done"
             except Exception as e:
-                scan_status[f"{repo},{second_branch}"] = f"failed,{str(e)}"                
+                report[second_branch] = {'scan_status': 'failed', 'scan_error_message': e}
+                # scan_status[f"{repo},{second_branch}"] = f"failed,{str(e)}"
+
+            scan_report[repo] = report
 
         finally:
-            scan_status_report_data = generate_scan_status_data(scan_status, first_branch, second_branch)
-            write_scan_status_report(f'{cwd}/output.xlsx', scan_status_report_data)
+            # Generate and status and export into result
+            generate_scan_status_data(scan_report, first_branch, second_branch)
+            # write_scan_status_report(f'{cwd}/output.xlsx', scan_status_report_data)
 
-    return scan_status
+    return scan_report
 
 
 # Return list of cloned repo name stored in /temp/repos dir
@@ -87,46 +98,81 @@ def generate_scan_status_data_for_file(repo_name, first_file, second_file):
     return scan_status_report_data
 
 
-def generate_scan_status_data(scan_status, first_branch, second_branch):
+def generate_scan_status_data(scan_report, first_branch, second_branch):
     scan_status_report_data = [[
         "Repo", "Branch", "scan status", "scan error", "comparison status", "comparison error", "unique flow count", "scan time", "CPG size"
     ]]
     cwd = os.getcwd()
-    
+
+    # create the empty excel file
     create_new_excel(f"{cwd}/output.xlsx", first_branch, second_branch)
 
-    for repo_branch, status in scan_status.items():
-        repo, branch = repo_branch.split(',')
-        status_breakdown = status.split(',')
-        error_message = "--"
+    for repo in scan_report.keys():
+        parse_flows_data(repo, first_branch, scan_report)
+        parse_flows_data(repo, second_branch, scan_report)
 
-        scan_metadata_regex = r".*(Code scanning|Binary file size|Deduplicating flows is done in).*"
-        scan_metadata_values = []
+    # for repo_branch, status in scan_status.items():
+    #     repo, branch = repo_branch.split(',')
+    #     status_breakdown = status.split(',')
+    #     error_message = "--"
+    #
+    #     scan_metadata_regex = r".*(Code scanning|Binary file size|Deduplicating flows is done in).*"
+    #     scan_metadata_values = []
+    #
+    #     with open(f"{cwd}/temp/result/{branch.strip()}/{repo}-output.txt") as scan_time_output:
+    #         for line in scan_time_output.readlines():
+    #             if re.search(scan_metadata_regex, line):
+    #                 scan_metadata_values.append(line)
+    #
+    #     unique_flows = ""
+    #     code_scan_time = ""
+    #     binary_file_size = ""
+    #
+    #     try:
+    #         unique_flows = scan_metadata_values[0].split('-')[-1]
+    #         code_scan_time = scan_metadata_values[1].split('-')[-2]
+    #         binary_file_size = scan_metadata_values[2].split('-')[-1]
+    #     except Exception as e:
+    #         print(f'Error during parsing the status data: {e}')
+    #
+    #     if len(status_breakdown) > 1:
+    #         error_message = status_breakdown[1]
+    #
+    #     comparison_status = "failed" if len(status_breakdown) > 1 else "done"
+    #     scan_status_report_data.append([repo, branch, status_breakdown[0], error_message, comparison_status, "--", unique_flows, code_scan_time, binary_file_size])
+    #
+    # scan_status_report_data.append([])
+    # return scan_status_report_data
 
-        with open(f"{cwd}/temp/result/{branch.strip()}/{repo}-output.txt") as scan_time_output:
-            for line in scan_time_output.readlines():
-                if (re.search(scan_metadata_regex, line)):
-                    scan_metadata_values.append(line)
+def parse_flows_data(repo_name, branch_name, scan_report):
 
-        unique_flows = ""
-        code_scan_time = ""
-        binary_file_size = ""
-        
-        try:
-            unique_flows = scan_metadata_values[0].split('-')[-1] 
-            code_scan_time = scan_metadata_values[1].split('-')[-2]
-            binary_file_size = scan_metadata_values[2].split('-')[-1]
-            
-        except:
-            print("Something went wrong")
+    cwd = os.getcwd()
 
+    scan_metadata_regex = r".*(Code scanning|Binary file size|Deduplicating flows is done in).*"
+    scan_metadata_values = []
 
-        if (len(status_breakdown) > 1):
-            error_message = status_breakdown[1]
+    with open(f"{cwd}/temp/result/{branch_name}/{repo_name}-output.txt") as scan_time_output:
+        for line in scan_time_output.readlines():
+            if re.search(scan_metadata_regex, line):
+                scan_metadata_values.append(line)
 
-        comparison_status = "failed" if len(status_breakdown) > 1 else "done"
-        scan_status_report_data.append([repo, branch, status_breakdown[0], error_message, comparison_status, "--", unique_flows, code_scan_time, binary_file_size])
-    
-    scan_status_report_data.append([])
-    return scan_status_report_data
+    try:
+        unique_flows = scan_metadata_values[0].split('-')[-1]
+        scan_report[repo_name][branch_name]['unique_flows'] = unique_flows
+    except Exception as e:
+        print(f'Error while parsing unique flow data: {e}')
+        scan_report[repo_name][branch_name]['unique_flows'] = '--'
 
+    try:
+        code_scan_time = scan_metadata_values[1].split('-')[-2]
+        scan_report[repo_name][branch_name]['code_scan_time'] = code_scan_time
+    except Exception as e:
+        print(f'Error while parsing code scan time data: {e}')
+        scan_report[repo_name][branch_name]['code_scan_time'] = '--'
+
+    try:
+        binary_file_size = scan_metadata_values[2].split('-')[-1]
+        scan_report[repo_name][branch_name]['binary_file_size'] = binary_file_size
+    except Exception as e:
+        print(f'Error while parsing binary file size data: {e}')
+        scan_report[repo_name][branch_name]['binary_file_size'] = '--'
