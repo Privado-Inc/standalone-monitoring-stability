2eb5d27df8525295f6ca7585a3c03b5d669e79c3
Karan <karan.batavia@privado.ai>
2023-02-15 15:59:02 +0530

    dv by 0 error fixed

--- utils/write_to_file.py
+++ utils/write_to_file.py
@@ -154,6 +154,7 @@ def write_summary_data(workbook_location, base_branch_name, head_branch_name, re
                           report[repo]['missing_sink'],
                          "---"])
 
+    # cannot divide by zero
     scan_time_positive_average = scan_time_positive_average / scan_time_positive if scan_time_positive > 0 else 0 # Average of more time repos
     scan_time_negative_average = scan_time_negative_average / (len(report.keys()) - scan_time_positive) * -1 if (len(report.keys()) - scan_time_positive) * -1 > 0 else 0 # Average of less time repos
 
