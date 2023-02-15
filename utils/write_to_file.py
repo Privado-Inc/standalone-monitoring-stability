a468d9b08098e0038a6b3c8c2c0eb5cf1b423f5a
Karan <karan.batavia@privado.ai>
2023-02-15 16:15:06 +0530

    dv by 0 error fixed

--- utils/write_to_file.py
+++ utils/write_to_file.py
@@ -129,7 +129,7 @@ def write_summary_data(workbook_location, base_branch_name, head_branch_name, re
         language = report[repo]['language']
         scan_time_diff = '--' if base_scan_time == '--' or head_scan_time == '--' else int(head_scan_time) - int(base_scan_time)
 
-        print("Scan time diff: " + scan_time_diff)
+        print("Scan time diff: " , scan_time_diff)
         if (scan_time_diff > 0): # Head branch took more time
             scan_time_positive += 1
             scan_time_positive_average += scan_time_diff
