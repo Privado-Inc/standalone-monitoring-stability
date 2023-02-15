5bdf74486d267311241a0c79da896e9ba89d3225
Karan <karan.batavia@privado.ai>
2023-02-15 12:33:37 +0530

    repo list change

--- run.py
+++ run.py
@@ -91,8 +91,8 @@ def workflow():
                 scan_status[repo_name][args.head]['comparison_error_message'] = '--'
 
                 source_data = process_sources(args.base, args.head, repo_name, detected_language) # Get the source data from the process_sources function
-                print(source_data)
-                source_count[repo_name] = {args.base: source_data[5], args.head: source_data[4]}
+                print("Source data: ", source_data)
+                source_count[repo_name] = dict({args.base: source_data[5], args.head: source_data[4]})
             except Exception as e:
                 print(f'{repo_name}: comparison report not generating: {e}')
                 scan_status[repo_name][args.base]['comparison_status'] = 'failed'
