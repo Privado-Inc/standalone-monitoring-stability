ed1d94bfa9422bf61b8da4671c5dc3307465cc88
Karan <karan.batavia@privado.ai>
2023-02-22 15:12:08 +0530

    debug

--- utils/compare.py
+++ utils/compare.py
@@ -355,7 +355,9 @@ def sub_process_path(base_source, head_source, sink_type, base_branch_name, head
         else:
             process_source_head_data[source_id] = sink_data
 
+    print("Base process source data")
     print(process_source_base_data)
+    print("Head process source data")
     print(process_source_head_data)
     source_union = set(process_source_head_data.keys()).union(set(process_source_base_data.keys()))
 
