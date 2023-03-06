a061c4789c2474294405e92a7b531af0d8e2ea1d
Ankit Kumar <ankit.kumar@privado.ai>
2023-03-07 00:41:22 +0530

    test

--- config.py
+++ config.py
@@ -41,8 +41,8 @@ def init(args):
 def resolve_core_branch_key_name(core_base_branch_name, core_head_branch_name, rules_base_branch_name, rules_head_branch_name, rule_compare):
     global BASE_CORE_BRANCH_KEY, HEAD_CORE_BRANCH_KEY, BASE_SHEET_BRANCH_NAME, HEAD_SHEET_BRANCH_NAME
 
-    BASE_CORE_BRANCH_KEY = f'{core_base_branch_name}${rules_base_branch_name}'
-    HEAD_CORE_BRANCH_KEY = f'{core_head_branch_name}${rules_head_branch_name}'
+    BASE_CORE_BRANCH_KEY = f'{core_base_branch_name}#{rules_base_branch_name}'
+    HEAD_CORE_BRANCH_KEY = f'{core_head_branch_name}#{rules_head_branch_name}'
 
     if rule_compare:
         BASE_SHEET_BRANCH_NAME = rules_base_branch_name
