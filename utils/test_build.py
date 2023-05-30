bc5fe299ab9e8fcecb9a07b582813d1e52676c7f
Karan <karan.batavia@privado.ai>
2023-05-30 11:34:16 +0530

    fix

--- utils/test_build.py
+++ utils/test_build.py
@@ -1,7 +1,7 @@
 import os
 
 def build():
-    path = f'{os.get_cwd()}/temp/privado-core'
+    path = f'{os.getcwd()}/temp/privado-core'
     os.system(f"cd {path} && sbt clean stage | tee {os.getcwd()}/temp/build_output.txt")
 
 build()
