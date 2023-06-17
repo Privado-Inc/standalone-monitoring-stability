501cdb37f447c4763ff10cd28e44c9bd4c482013
Ankit Kumar <ankit.kumar@privado.ai>
2023-06-17 21:38:57 +0530

    joern-fix

--- builder.py
+++ builder.py
@@ -34,4 +34,4 @@ def get_joern_privado_path(path):
 
 
 def get_privado_path(version):
-    return f'{pwd}/temp/privado/Privado-Inc-{version}#Privado-Inc-dev'
+    return f'{pwd}/temp/privado/Privado-Inc-{version}@Privado-Inc-dev'
