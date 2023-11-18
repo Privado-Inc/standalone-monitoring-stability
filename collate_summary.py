76d74264fc3a57d8d07ca30e612e6349a45d0866
Ankit Kumar <ankit.kumar@privado.ai>
2023-11-18 10:42:25 +0530

    Added go repos

--- collate_summary.py
+++ collate_summary.py
@@ -281,6 +281,7 @@ def main():
     summary = ""
 
     for language_summary in get_file_contents(args.summary_dir):
+
         if args.joern_update:
             joern_version.get_result(language_summary)
             language_summary = language_summary[2:]
