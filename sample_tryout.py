442dee535e4e7b102c6c6e6096a04248e343720b
Karan Batavia <karan.batavia@privado.ai>
2023-12-07 20:05:55 +0530

    test

--- sample_tryout.py
+++ sample_tryout.py
@@ -1,7 +1,7 @@
 import subprocess
 
 
-process_1 = subprocess.Popen(["privado", "scan", "~/Privado/repos/java/sample_java", "--overwrite"])
+process_1 = subprocess.Popen(['export', '_JAVA_OPTIONS=-Xmx14G', '&&', 'cd', '/home/ubuntu/standalone-monitoring-stability/temp/binary/Privado-Inc-main@Privado-Inc-main/bin', '&&', './privado-core', 'scan', '/home/ubuntu/standalone-monitoring-stability/temp/repos/Library-Assistant', '-ic', '/home/ubuntu/standalone-monitoring-stability/temp/privado/Privado-Inc-main@Privado-Inc-main', '--skip-upload', '--test-output', '2>&1', '|', 'tee', '-a', '/home/ubuntu/standalone-monitoring-stability/temp/result/Privado-Inc-main@Privado-Inc-main/Library-Assistant-output.txt'])
 process_2 = subprocess.Popen(["privado", "scan", "~/Privado/repos/java/sample_java", "--overwrite"])
 
 
