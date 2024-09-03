37591a55180dcc9bbedbe46d8c6842e172f3acec
karan-batavia <karan.batavia@privado.ai>
2024-09-03 17:44:03 +0530

    enable curl logs

--- slack_upload.sh
+++ slack_upload.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Start the upload action by getting a file upload URL in the response
-response=$(curl -s -F files=@$FILE_NAME \
-    -F filename=$FILE_NAME \
+response=$(curl -s -F files=@$FILE_PATH \
+    -F filename=$FILE_PATH \
     -F token=$SLACK_TOKEN \
-    -F length=$(stat --format=%s $FILE_NAME | tr -d '\n') \
+    -F length=$(stat --format=%s $FILE_PATH | tr -d '\n') \
        https://slack.com/api/files.getUploadURLExternal)
 
 
@@ -12,16 +12,15 @@ upload_url=$(echo "$response" | jq -r '.upload_url')
 file_id=$(echo "$response" | jq -r '.file_id')
 
 # Upload the file
-curl -s -o /dev/null -F filename="@$FILE_NAME" -H "Authorization: Bearer $SLACK_TOKEN" POST $upload_url
-
+curl -s -o /dev/null -F filename="@$FILE_PATH" -H "Authorization: Bearer $SLACK_TOKEN" POST $upload_url
 
 # Finalize the upload
-curl -X POST \  -H "Authorization: Bearer $SLACK_TOKEN" \
+curl -X POST \ -H "Authorization: Bearer $SLACK_TOKEN" \
   -H "Content-Type: application/json" \
   -d '{
         "initial_comment": "'$PR_MESSAGE'",
         "thread_ts": "'$INIT_TS'",
-        "files": [{"id": "'$file_id'", "title":"'$'"}],
+        "files": [{"id": "'$file_id'", "title":"'$FILE_NAME'"}],
         "channel_id": "'$SLACK_CHANNEL_ID'"
       }' \
-  https://slack.com/api/files.completeUploadExternal > /dev/null
+  https://slack.com/api/files.completeUploadExternal
